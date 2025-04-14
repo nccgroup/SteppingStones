821762e04dadae8b57ef8c81dccb91ec58a44944
Stephen Tomkinson <stephen.tomkinson@gmail.com>
2025-04-14 10:22:40 +0100

    Replace deprecated sqlite_attach duckdb function with new ATTACH syntax

--- event_tracker/__init__.py
+++ event_tracker/__init__.py
@@ -3,4 +3,5 @@ import duckdb
 
 if os.path.exists("db.sqlite3"):
     duckdb.sql("INSTALL sqlite; LOAD sqlite; SET GLOBAL sqlite_all_varchar=true;")
-    duckdb.sql("CALL sqlite_attach('db.sqlite3');")
+    duckdb.sql("ATTACH 'db.sqlite3' (TYPE sqlite);")
+    duckdb.sql("USE db;")
