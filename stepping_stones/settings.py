425adb7813198760311a6725ac213a13ee9b1f74
Stephen Tomkinson <stephen.tomkinson@gmail.com>
2026-03-25 12:06:09 +0000

    Enforce standard Django model secuirity at the API layer

--- stepping_stones/settings.py
+++ stepping_stones/settings.py
@@ -230,6 +230,7 @@ REST_FRAMEWORK = {
                                        'rest_framework.authentication.SessionAuthentication',),
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.DjangoModelPermissions'
     ]
 }
 
